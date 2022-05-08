5a8273421e0203d30b853b406818cdb31867dd03
Christoffer Kjeldgaard Petersen <c.kjeldgaard.petersen@gmail.com>
2022-05-08 17:35:46 +0200

    Typo

--- rider.go
+++ rider.go
@@ -3,7 +3,7 @@ package main
 type rider struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
-	Nationality string `json:"nationaliy"`
+	Nationality string `json:"nationality"`
 	Age         int    `json:"age"`
 	Team        string `json:"team"`
 }
